99b371ebb7434af89cdb6dab4dfbab9927cdbe8a
chenhao <wb_chenhao26@meituan.com>
2025-09-09 16:53:12 +0800

    init:删除无用代码2

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,12 +1,2 @@
 // 导出HTTP客户端
 export { request } from './axios'
-
-// 导出具体API模块
-export { uploadApi } from './upload'
-
-// 统一导出所有API
-import { uploadApi } from './upload'
-
-export const api = {
-  upload: uploadApi
-} 
